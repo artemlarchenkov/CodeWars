bc7ec6662f89cf4d5478fa5c01dbbf2cc8bf542b
artemlarchenkov <artem.bond.2000@mail.ru>
2024-01-29 21:08:06 +0300

    Create Count by X.cpp

---
+++ 8kuy/Count by X.cpp
@@ -0,0 +1,26 @@
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+// countBy(1,10)  should return  {1,2,3,4,5,6,7,8,9,10}
+// countBy(2,5)  should return {2,4,6,8,10}
+
+// My Solution:
+
+#include <vector>
+std::vector<int> countBy(int x,int n){
+
+  std::vector<int> z;
+  if(x == 0){return {};}
+  if(n == 0){return {};}
+  
+  int i;
+  for(i=1;i<=n;i++){
+    z.push_back(i*x);
+  }
+  
+  return z;
+}
